9e0f007b4f65c4ad2f0f7976f3a1ac3b947980dc
Nikema Prophet <3941856+prophen@users.noreply.github.com>
2026-01-08 02:03:59 -0800

    Implement color scheme generation and update color display

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+let schemeArr = [];
+
+const generateBtn = document.getElementById("generate-scheme");
+
+function displayColors() {
+  const colorsContainer = document.querySelector(".colors");
+  schemeArr.forEach((colorObj, index) => {
+    const colorDiv = colorsContainer.querySelector(`.color-${index}`);
+    colorDiv.style.backgroundColor = colorObj.hex.value;
+  });
+  const hexCodesContainer = document.querySelector(".hex-codes");
+  schemeArr.forEach((colorObj, index) => {
+    const hexDiv = hexCodesContainer.querySelectorAll(".hex-code")[index];
+    hexDiv.textContent = colorObj.hex.value;
+  });
+}
+generateBtn.addEventListener("click", () => {
+  const baseColor = document.getElementById("base-color").value.slice(1);
+  const schemeMode = document.getElementById("scheme-mode").value;
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=${schemeMode}&count=5`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      schemeArr = data.colors;
+      displayColors();
+    });
+});
